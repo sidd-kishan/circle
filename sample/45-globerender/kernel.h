e32ddd31d279019c91cb4efdd09496671e68c3e3
Aayush Deo <aayush.deo1994@gmail.com>
2026-01-11 16:12:27 +0530

    Update kernel.h with new includes and rendering methods
    Added license header, updated and expanded include directives, and introduced new private rendering-related methods and members to CKernel. Also replaced C2DGraphics with CScreenDevice and CDeviceNameService, and added comments clarifying member order.

--- sample/45-globerender/kernel.h
+++ sample/45-globerender/kernel.h
@@ -1,32 +1,71 @@
+//
+// kernel.h
+//
+// Circle - A C++ bare metal environment for Raspberry Pi
+// Copyright (C) 2014  R. Stange <rsta2@o2online.de>
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 #ifndef _kernel_h
 #define _kernel_h
 
 #include <circle/actled.h>
 #include <circle/koptions.h>
-#include <circle/2dgraphics.h>
+#include <circle/devicenameservice.h>
+#include <circle/screen.h>
 #include <circle/types.h>
-
+#include "./penger.h"
+#include <circle/serial.h>
+#include <circle/exceptionhandler.h>
+#include <circle/interrupt.h>
+#include <circle/timer.h>
+#include <circle/logger.h>
+#include <circle/usb/usbhcidevice.h>
+#include <circle/usb/usbkeyboard.h>
 enum TShutdownMode
 {
-    ShutdownNone,
-    ShutdownHalt,
-    ShutdownReboot
+	ShutdownNone,
+	ShutdownHalt,
+	ShutdownReboot
 };
 
 class CKernel
 {
 public:
-    CKernel (void);
-    ~CKernel (void);
+	CKernel (void);
+	~CKernel (void);
 
-    boolean Initialize (void);
-    TShutdownMode Run (void);
+	boolean Initialize (void);
+
+	TShutdownMode Run (void);
 
 private:
-    // ORDER MATTERS IN CIRCLE
-    CActLED         m_ActLED;
-    CKernelOptions m_Options;
-    C2DGraphics    m_2DGraphics;
+	// Rendering functions
+	void Clear (void);
+	Point2D Screen (const Vector3D &p);
+	Vector3D Project (const Vector3D &p);
+	Vector3D TranslateZ (const Vector3D &v, float dz);
+	Vector3D RotateXZ (const Vector3D &v, float angle);
+	void DrawLine (int x1, int y1, int x2, int y2);
+
+private:
+	// do not change this order
+	CActLED			m_ActLED;
+	CKernelOptions		m_Options;
+	CDeviceNameService	m_DeviceNameService;
+	CScreenDevice		m_Screen;
+	
 };
 
 #endif
